a03fa64a2e063aaf4d67a599342e4314a46f7643
Andres-is.github.io <132363688+Andres-is@users.noreply.github.com>
2024-02-04 20:35:12 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import cors from "cors"
 
 import employesRoutes from './routes/employees.routes.js'
 
@@ -11,9 +11,11 @@ const app = express()
 
 
 app.use(express.json())
-
+app.use(cors())
 app.use(indexRoutes)
 app.use('/api',employesRoutes)
+
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     res.status(404).json({
